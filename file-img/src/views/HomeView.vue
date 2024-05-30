74b37b75f777a357f735b5660670821fcead24ec
gu-yue-fang-yuan1 <gu-yue-fang-yuan1>
2024-05-30 16:37:05 +0800

    lianjie

---
+++ file-img/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <input id="file" type="file" @input="fileChange">
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ref from 'vue';
+const fileChange:any = function (e:File) {
+  console.log(e);
+}
+</script>
