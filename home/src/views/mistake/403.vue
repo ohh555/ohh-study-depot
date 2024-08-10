ab3a3103200e1abb8255a87f1a484dcf36b64806
gu-yue-fang-yuan1 <gu-yue-fang-yuan1>
2024-08-10 21:04:52 +0800

    同步2

---
+++ home/src/views/mistake/403.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>403</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ home/src/views/mistake/404.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>404</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
